3b45f46ec6b755f144fa08b72716facb5cf5a73b
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-17 21:38:13 +0530

    [chiru] Checking Maximum String

--- Generics_New/Program.cs
+++ Generics_New/Program.cs
@@ -4,11 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Find Maximum Float Number");
+            Console.WriteLine("Find Maximum String");
             //int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
             //Console.WriteLine(output);
-            double doubleoutput = MaximumFloatCheck.MaximumFloatNumber(11.22, 22.33, 33.44);
-            Console.WriteLine(doubleoutput);
+
+            //double doubleoutput = MaximumFloatCheck.MaximumFloatNumber(11.22, 44.33, 33.44);
+            //Console.WriteLine(doubleoutput);
+
+            string stringoutput = MaximumStringCheck.MaximumString("Ram", "Prabu", "Revanth");
+            Console.WriteLine(stringoutput);
 
         }
     }
