4b18d733151cdd0e3c09375bab7f78cf97c487b6
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-17 21:13:48 +0530

    [chiru] Checking maximum Number

---
+++ Generics_New/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Generics_New
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Maximum Number");
+            int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
+            Console.WriteLine(output);
+        }
+    }
+}
