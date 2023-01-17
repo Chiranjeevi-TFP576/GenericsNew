04ac3d8671f746f701017e8acdbd89115c918204
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-17 22:12:20 +0530

    [chiru] Checking GenericMaximum Number

--- Generics_New/Program.cs
+++ Generics_New/Program.cs
@@ -4,15 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Find Maximum String");
+            Console.WriteLine("Generic Maximum Number");
             //int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
             //Console.WriteLine(output);
 
             //double doubleoutput = MaximumFloatCheck.MaximumFloatNumber(11.22, 44.33, 33.44);
             //Console.WriteLine(doubleoutput);
 
-            string stringoutput = MaximumStringCheck.MaximumString("Ram", "Prabu", "Revanth");
-            Console.WriteLine(stringoutput);
+            //string stringoutput = MaximumStringCheck.MaximumString("Ram", "Prabu", "Revanth");
+            //Console.WriteLine(stringoutput);
+
+            int[] arr = { 112, 344, 432, 555, 678 };
+            GenericMaximumNumber<int> generic = new GenericMaximumNumber<int>(arr);
+            generic.PrintMaxValue();
+            
+            double[] doublearray = { 11.2, 34.4, 43.2, 55.5, 67.8 };
+            GenericMaximumNumber<double> genericDouble = new GenericMaximumNumber<double>(doublearray);
+            genericDouble.PrintMaxValue();
 
         }
     }
